325ddec5e5333952b81e2a85985df510c34fd247
Akshit Khajuria <khajurination@gmail.com>
2020-09-03 13:51:47 +0530

    Added route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const forget_password = require("../modules/forget_password");
 
+// Also has a validate middleware for authorising token on protected routes
+var loginHandler = require('../modules/logIn');
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -9,5 +12,6 @@ const forget_password = require("../modules/forget_password");
 
 router.route("/forgot_Password").post(forget_password.forgot_Password);
 
+router.get('/login', loginHandler.login);
 
 module.exports = router;
