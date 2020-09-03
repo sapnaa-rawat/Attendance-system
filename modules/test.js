150deaad8f9bda04f25c6615d72c75f3691a373e
pawanjoshi2312 <pawan.joshi.2312@gmail.com>
2020-09-03 20:42:43 +0530

    weeklyattendancecheck

--- modules/test.js
+++
@@ -1,6 +0,0 @@
-var moment = require('moment');
-var today=moment().format('DD-MMM-YYYY');
-var day = '03-sep-2020';
-var exitsdate = moment(day, 'DD-MMM-YYYY').isAfter('31-jul-2020');
-var tomorrow=moment(today).add(1,'days');
-console.log(exitsdate);
--- modules/weeklyAttendance.js
+++ modules/weeklyAttendance.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const dataModel = require('../model/datamodel');
+const resourceModel = require('../model/resource');
+const holidayModel = require('../model/holiday')
 
+
+const authenticateToken = (req, res, next) => {
+    
+    try {
+        
+         const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, "req.session.privatekey");
+        req.user = {
+            email: decodedToken.email
+        };
+        next();
+    } catch (err) {
+        res.status(401).json({
+            message: "Auth failed!"
+        });
+    }
+
+}
 
 
 function findIdfromemail(req, res, next) {
     let email = req.user.email;
 
-    empdata.findOne({
+    resourceModel.findOne({
         "email": email
     }).exec(function (error, response) {
         if (error) return res.status(422).send("something went wrong")
@@ -16,42 +35,61 @@ function findIdfromemail(req, res, next) {
     })
 }
 
-function is_notweekend(req,res,next) {
+function is_notweekend(req, res, next) {
     let dateforsearch = req.body.date;
     var dt = new Date(dateforsearch);
     var varifyDate = moment(dateforsearch, 'DD-MMM-YYYY').isAfter('31-jul-2020')
 
-    if (dt.getDay() == 1 && varifyDate==true) {
+    if (dt.getDay() == 1 && varifyDate == true) {
         next();
     }
-    return res.status(422).send({msg:'not a valid date to find attendance of whole week'});
+    return res.status(422).send({
+        msg: 'not a valid date to find attendance of whole week'
+    });
 }
 
-function is_holiday(req,res,next){
+/* function is_holiday(req, res, next) {
     let dateforsearch = req.body.date;
-    
-}
+
+    holidayModel.find({
+        'holidayDate': dateforsearch
+    }).exec(function (error, data) {
+        if (error) {
+            return res.status(422).send("something went wrong");
+        }
+        if (data) {
+            return res.status(200).send({
+                msg: data.occasion
+            })
+        }
+        next()
+    })
+} */
 
 function weeklyAttendance(req, res, next) {
     let id = req.id;
     let dateforsearch = req.body.date;
-    dataModel.find({
-        'id': id,
-        'date':dateforsearch
-    }).exec(function(error,data){
-        if(error){
-            return res.status(422).send({msg:'something went wrong'})
-        }
-        var datefromdb=data.date;
-    })
+    for (let i = 0; i < 5; i++) {
+        dataModel.find({
+            'id': id,
+            'date': dateforsearch
+        }).exec(function (error, data) {
+            if (error) {
+                return res.status(422).send({
+                    msg: 'something went wrong'
+                })
+            }
+            console.log(data.date+'>>>>>'+data.attendance);
+        })
+         dateforsearch = moment(dateforsearch).add(1, 'days');
+    }
 
-    
 
-    
 }
 
 module.exports = {
     weeklyAttendance,
     is_notweekend,
-    findIdfromemail
+    findIdfromemail,
+    is_holiday
 }
