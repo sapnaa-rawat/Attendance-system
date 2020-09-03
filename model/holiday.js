37490cbf147d6f9122a2898039654e177f335671
Ankit Prasad <ankitprasad7@users.noreply.github.com>
2020-09-03 22:13:14 +0530

    update

--- model/holiday.js
+++ model/holiday.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var list = new Schema({
-    holidayDate: { type: Date},
-    occasion:{type: String}
+    holidayDate:['01-Jan-2020','21-Feb-2020','10-Mar-2020','10-Apr-2020','25-May-2020','03-Aug-2020','02-Oct-2020','13-Nov-2020','30-Nov-2020','25-Dec-2020']
+    
 })
 var holidayList = mongoose.model("list", list);
 module.exports = holidayList;
