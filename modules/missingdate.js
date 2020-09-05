686ca4a6bcdce6563afdcc551f969833d7bdcc65
mahesh garkhwal <mahesh.garkhwal@gmail.com>
2020-09-05 15:43:00 +0530

    missing attendance added

--- modules/missingdate.js
+++ modules/missingdate.js
@@ -6,6 +6,7 @@ async function missing(req, res, next){
 
     var holidays=['03-aug-2020', '02-oct-2020', '13-nov-2020', '30-nov-2020', '25-dec-2020'];
     var missingdates=[];
+    var key2=false;
 
          var fdate = "2020-07-31";
         var fromdate = moment(fdate).format('DD-MMM-YYYY');
@@ -13,8 +14,17 @@ async function missing(req, res, next){
         var tdate = new Date();
          var todate=moment(tdate).format('DD-MMM-YYYY');
 
+         var data1=await attendanceschema.find();
+         var temp=[];
+         data1.forEach((record)=>{temp.push(record.empid)});
+        let length=temp.length;
+
         try{
-                for (var m = moment(fromdate); m.isBefore(todate); m.add(1, 'days')) {
+
+            for(start=0; start<temp.length; start++)
+            {
+                idemp=temp[start];
+                for (var m = moment(fromdate); m.isSameOrBefore(todate); m.add(1, 'days')) {
                     var hd=null;
                     let key=false;
                     let day=m.day();
@@ -31,29 +41,33 @@ async function missing(req, res, next){
                         if(key===false){
                         
                         if(id!=undefined){
-                        var data=await attendanceschema.findOne({$and:[{empid: id, date:m.format("DD-MMM-YYYY")}]});
-                            if(data==null){
-                            var obj = {};
-                            obj[id]=m.format('DD-MMM-YYYY')
-                            missingdates.push(obj);
+                            if(key2==false){   
+                            var data=await attendanceschema.findOne({$and:[{empid: id, date:m.format("DD-MMM-YYYY")}]});
+                                if(data==null){
+                                var obj = {};
+                                obj[id]=m.format('DD-MMM-YYYY')
+                                missingdates.push(obj);
+                                }
                             }
-                        
                             }
+    
                             if(id===undefined){
-                                var data=await attendanceschema.find({project:true});
-                                    if(data==null){
+                                   var data=await attendanceschema.find({$and:[{empid: idemp,project:true, date:m.format("DD-MMM-YYYY")}]});
+                                   if(data.length==0){
                                         var obj = {};
-                                        obj[id]=m.format('DD-MMM-YYYY')
-                                        missingdates.push(obj);
+                                        obj[idemp]=m.format('DD-MMM-YYYY')
+                                        missingdates.push(obj);            
                                         }
                              }
                       }
-                    }
-                }
-                console.log(typeof(id));
-                console.log(id);
+                 }
+                
+            }
+            key2=true;
+        }
                 console.log(missingdates);
                 res.status(200).json({"empid":missingdates});
+            
         }
         catch(err){
         console.log(err);
