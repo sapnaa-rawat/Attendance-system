3f23eb0d74b5299f46efbef33ae73507744f728f
Akshit Khajuria <khajurination@gmail.com>
2020-09-10 00:51:32 +0530

    enhanced leaves module- find leaves in any range

--- modules/leaves.js
+++ modules/leaves.js
@@ -6,35 +6,22 @@ const leaves = ['PL', 'UPL', 'UCL', 'PCL'];
 var holidays = ['03-Aug-2020', '02-Oct-2020', '13-Nov-2020', '30-Nov-2020', '25-Dec-2020'];
 
 async function getLeaveOnDate(req, res) {
-    var date = req.body.date;
+    var date = moment(req.body.date);
     var empid = req.body.empid;
-    var now = moment(date, 'DD-MMM-YYYY');
 
-    if (now.isValid()) {
-        var week = now.day();
+    if (date.isValid()) {
+        var week = date.day();
         if (week == 0 || week == 6) {
-            res.status(200).json({ message: "no data for saturday and sunday" })
-
+            res.status(200).json({ message: "no data for saturday and sunday" });
         }
         else {
-            if (empid === undefined) {
-                var result = await empattendance.find({ "date": date, "project": true, "empattendance": { $in: leaves } });
-
-                if (result.length === 0) {
-                    return res.status(404).json({ message: "No leaves record found" });
-                }
-                var temp = result.map(key => { return { "empid": key.empid, "leave": key.empattendance, "date": key.date } });
-                return res.status(200).json(temp);
-            }
-            var result = await empattendance.find({ "empid": empid, "date": date, "empattendance": { $in: leaves } });
-
+            var result = await getLeavesFrom(empid, date.format('DD-MMM-YYYY'), 1);
             if (result.length === 0) {
-                return res.status(404).json({ message: "No leaves record found" });
+                res.status(404).json({ message: "No leaves record found" });
             }
-
-            res.status(200).json({ "empid": result[0].empid, "leave": result[0].empattendance, "date": result[0].date });
-
-
+            else {
+                res.status(200).json(result);
+            }
         }
     }
 
@@ -43,23 +30,49 @@ async function getLeaveOnDate(req, res) {
     }
 }
 
-async function getWeeklyLeaves(req, res) {
-    const date = req.body.date;
-    const _date = moment(req.body.date, 'DD-MMM-YYYY');
+async function getLeavesFrom(id, sdate, days) {
+    const date_end = moment(sdate).add(parseInt(days), 'days').format('DD-MMM-YYYY');
+    if (!id) {
+        var leavesList = await empattendance.find({ project: true, date: { $gte: sdate, $lt: date_end, $nin: holidays }, "empattendance": { $in: leaves } }).sort({ date: 1 });
+    }
+    else {
+        leavesList = await empattendance.find({ empid: id, date: { $gte: sdate, $lt: date_end, $nin: holidays }, "empattendance": { $in: leaves } }).sort({ date: 1 });
+    }
+    if (leavesList.length === 0) {
+        return [];
+    }
+    var result = leavesList.map(key => { return { "empid": key.empid, "leave": key.empattendance, "date": key.date } });
+    return result;
+}
+
+async function getmonthlyLeaves(req, res) {
+    const _date = moment(req.body.date);
     const id = req.body.id;
-    const date_end = _date.clone().add(5, 'days').format('DD-MMM-YYYY');
     if (_date.isValid()) {
-        if (!id) {
-            var leavesList = await empattendance.find({ project: true, date: { $gte: date, $lte: date_end, $nin: holidays }, "empattendance": { $in: leaves } }).sort({ date: 1 });
+        var result = await getLeavesFrom(id, _date.format('DD-MMM-YYYY'), 31);
+        if (result.length === 0) {
+            res.status(404).json({ message: "No leaves record found" });
         }
         else {
-            leavesList = await empattendance.find({ empid: id, date: { $gte: date, $lte: date_end, $nin: holidays }, "empattendance": { $in: leaves } }).sort({ date: 1 });
+            res.status(200).json(result);
         }
-        if (leavesList.length === 0) {
-            return res.status(404).json({ message: "No leaves record found" });
+    }
+    else {
+        res.status(400).json({ message: "invalid Date" });
+    }
+}
+
+async function getWeeklyLeaves(req, res) {
+    const _date = moment(req.body.date);
+    const id = req.body.id;
+    if (_date.isValid()) {
+        var result = await getLeavesFrom(id, _date.format('DD-MMM-YYYY'), 6);
+        if (result.length === 0) {
+            res.status(404).json({ message: "No leaves record found" });
         }
-        var result = leavesList.map(key => { return { "empid": key.empid, "leave": key.empattendance, "date": key.date } });
-        return res.status(200).json(result)
+        else {
+            res.status(200).json(result);
+        }
     }
     else {
         res.status(400).json({ message: "invalid Date" });
