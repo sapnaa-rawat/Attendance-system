5becf5737f84a852bddada7fe1f909d4b99aed2b
Akshit Khajuria <khajurination@gmail.com>
2020-09-09 18:35:23 +0530

    no leaves model only fetch data from attendance

--- modules/leaves.js
+++ modules/leaves.js
@@ -1,32 +1,43 @@
-var leaves = require('../model/leavesModel');
-var resource = require('../model/resource');
+var empattendance = require('../model/attendance');
 
-async function applyLeave(req, res) {
-    const day = req.body.date;
-    // Got email from validated token stored in req.user
-    const email = req.user.email;
-    try {
-        const { id } = await resource.findOne({ email: email });
-        const leave = new leaves({
-            empid: id,
-            appliedOn: day
-        });
-        leave.save()
-            .then(doc => res.status(201).send({ message: "Leave applied." }))
-            .catch(err => res.status(500).send({ message: "An error occured.", error: err }));
-    } catch (error) {
-        res.status(404).send({ message: "Employee id found." });
+const leaves = ['PL', 'UPL', 'UCL', 'PCL'];
+
+async function getLeaveOnDate(req, res) {
+    var date = req.body.date;
+    var empid = req.body.empid;
+    var now = moment(date, 'DD-MMM-YYYY');
+
+    if (now.isValid()) {
+        var week = now.day();
+        if (week == 0 || week == 6) {
+            res.status(200).json({ message: "no data for saturday and sunday" })
+
+        }
+        else {
+            if (empid === undefined) {
+                var result = await empattendance.find({ "date": date, "project": true, "empattendance": { $in: leaves } });
+
+                if (result.length === 0) {
+                    return res.status(404).json({ message: "No leaves record found" });
+                }
+                var temp = result.map(key => { return { "empid": key.empid, "status": key.empattendance, "date": key.date } });
+                return res.status(200).json(temp)
+            }
+            var result = await empattendance.find({ "empid": empid, "date": date, "empattendance": { $in: leaves } });
+
+            if (result.length === 0) {
+                return res.status(404).json({ message: "attendance not filled" });
+            }
+
+            res.status(200).json({ "empid": result[0].empid, "status": result[0].empattendance, "date": result[0].date });
+
+
+        }
+    }
+
+    else {
+        res.status(400).json({ message: "invalid Date" })
     }
 }
 
-async function getAll(req, res) {
-    try {
-        var allLeaves = await leaves.findAllLeavesById(req.user.id);
-        res.status(200).send({ leaves: allLeaves });
-    }
-    catch (error) {
-        res.status(500).send({ message: "An error occured.", error: `${error}` });
-    }
-}
-
-module.exports = { applyLeave, getAll };
+module.exports = { getLeaveOnDate };
