d2f29f674ebb56dd55e510c455ecf6677b8f6400
Akshit Khajuria <khajurination@gmail.com>
2020-09-11 23:16:23 +0530

    minor changes - formatting applied, ES6 syntax for missing

--- modules/missingdate.js
+++ modules/missingdate.js
@@ -11,7 +11,7 @@ const resources = require('../model/resource');
  * @returns array of dates
  */
 
- 
+
 const missingdates = (req, res) => {
     var id = req.body.empid;
     var to_Date = moment(new Date()).format('DD-MMM-YYYY');
@@ -27,7 +27,7 @@ const missingdates = (req, res) => {
             return item
         }
     });
-    if(id){
+    if (id) {
         attendance.find({ "empid": id, "date": filtered_holidays_Dates, "project": true }, (err, result) => {
             if (err) { return err }
             var filtered__Dates = filtered_holidays_Dates.filter((dates) => {
@@ -44,32 +44,32 @@ const missingdates = (req, res) => {
     }
     else {
         resources.find({ "project": true }, (err, resource) => {
-        const empid = resource.map((item) => {
-            return item.id;
-        })
-        attendance.find({ "empid": empid, "date": filtered_holidays_Dates, "project": true }, (err, result) => {
-            if (err) { return err }
-            var emp = [];
-            resource.forEach((user) => {
-                var filtered_users = result.filter((att) => {
-                    return user.id == att.empid;
-                })
-                var filtered__Dates = filtered_holidays_Dates.filter((dates) => {
-                    var status = true;
-                    filtered_users.forEach((item) => {
-                        if (dates == item.date) {
-                            status = false;
-                        }
-                    })
-                    return status;
-                })
-                let data = { "id": user.id, "Missing Dates": filtered__Dates }
-                emp.push(data);
+            const empid = resource.map((item) => {
+                return item.id;
             })
-            res.send(emp);
+            attendance.find({ "empid": empid, "date": filtered_holidays_Dates, "project": true }, (err, result) => {
+                if (err) { return err }
+                var emp = [];
+                resource.forEach((user) => {
+                    var filtered_users = result.filter((att) => {
+                        return user.id == att.empid;
+                    })
+                    var filtered__Dates = filtered_holidays_Dates.filter((dates) => {
+                        var status = true;
+                        filtered_users.forEach((item) => {
+                            if (dates == item.date) {
+                                status = false;
+                            }
+                        })
+                        return status;
+                    })
+                    let data = { "id": user.id, "Missing Dates": filtered__Dates }
+                    emp.push(data);
+                })
+                res.send(emp);
+            })
         })
-    })
-}
+    }
 
 }
 
@@ -87,50 +87,50 @@ function difference(setA, setB) {
     return _difference
 }
 
-async function newMissingDates(req, res) {
-    const {empid} = req.body;
-    const startingDate = moment([2020,6,31]);
+var newMissingDates = async (req, res) => {
+    const { empid } = req.body;
+    const startingDate = moment([2020, 6, 31]);
     const endDate = moment(new Date()); // 31-Jul-2020
     var day = startingDate.clone();
     const df = 'DD-MMM-YYYY'; // date format
     var calendar = new Set();
     // create a claendar till today, exclude holidays
-    for (var i = 0; i < endDate.diff(startingDate,'days'); i++) {
-        day = day.add(1,'days');
+    for (var i = 0; i < endDate.diff(startingDate, 'days'); i++) {
+        day = day.add(1, 'days');
         // check Sat Sun
-        if (day.day()===0 || day.day()===6){
+        if (day.day() === 0 || day.day() === 6) {
             continue;
         }
-        else if (constants.constant_Data.HOLIDAYS_DATE.includes(day.format(df))){
+        else if (constants.constant_Data.HOLIDAYS_DATE.includes(day.format(df))) {
             continue;
         }
-        else{
+        else {
             calendar.add(day.format(df));
         }
     }
     // search criteria for query
     var filter = {};
-    if(empid){
+    if (empid) {
         // if empid is provided, the query searches for that empid only, else gets all data
-        filter['empid']=empid;
+        filter['empid'] = empid;
     }
     // ALL attendances for each resource id - 
     var empAttendances = {};
     // ALL missing dates for each resource id - 
     var empMissingDate = {};
     var allUserData = await attendance.find(filter)
-        .then(allUserData=>{
+        .then(allUserData => {
             // segregate data for each empid bucket
-            allUserData.forEach(doc=>{
+            allUserData.forEach(doc => {
                 let id = doc.empid;
                 let date = doc.date;
-                if(!empAttendances[id]){
+                if (!empAttendances[id]) {
                     empAttendances[id] = new Set();
                 }
                 empAttendances[id].add(date);
             });
         })
-        .then(val=>{
+        .then(val => {
             // Now remove attendance dates from calendar dates for each empid
             // The remaining dates are the missing dates 
             for (const key in empAttendances) {
@@ -139,19 +139,19 @@ async function newMissingDates(req, res) {
                 }
             }
         })
-        .then(val=>{
+        .then(val => {
             // dates found, now send response
-            if(Object.keys(empMissingDate).length===0){
-                res.status(404).send({message:"No missing dates found."});
+            if (Object.keys(empMissingDate).length === 0) {
+                res.status(404).send({ message: "No missing dates found." });
             }
-            else{
+            else {
                 res.status(200).send(empMissingDate);
             }
         })
-        .catch(err=>res.status(500).send({error:`${err}`}));
+        .catch(err => res.status(500).send({ error: `${err}` }));
 }
 
-module.exports={
+module.exports = {
     missingdates,
     newMissingDates
 }
