231ead529ac73ae57f8a8fb992561f01eecd6392
Akshit Khajuria <khajurination@gmail.com>
2020-10-07 12:43:33 +0530

    Fixed markAttendance bug that returned null arrays as result

--- modules/attendance-record.js
+++ modules/attendance-record.js
@@ -9,7 +9,7 @@ const markAttendance = async (req, res) => {
       return await attendances.findOneAndUpdate({
         "empid": item.empid,
         "date": item.date
-      }, {$set: {empattendance: item.empattendance}}, {upsert: true});
+      }, {$set: {empattendance: item.empattendance}}, {upsert: true, returnOriginal: false});
   });
   Promise.all(queryMap).then(([...results]) => res.status(200).send({"message":"ok",results})).catch((err) => res.status(200).send(err));
 }
